d32ac12ebcaac4c741deafec9000849e7ea726fe
agent12 <imnova1212@gmail.com>
2023-07-01 18:34:00 +0700

    multiquery attention

--- transformers/attention.py
+++ transformers/attention.py
@@ -101,3 +101,41 @@ def test_forward_MultilHeadAttention():
         _test_forward_MultilHeadAttention(**kwargs)
     end = time()
     print('runtime = ', end-start)
+
+class  MultiQueryAttention(Attention):
+    def __init__(self, word_size: int = 512, embed_dim: int = 64, n_query:int=8) -> None:
+        super().__init__(word_size, embed_dim)
+        self.n_query = n_query
+        self.W0 = nn.Parameter(torch.empty(embed_dim * n_query, embed_dim))
+        nn.init.xavier_normal_(self.W0)
+        delattr(self, 'query')
+        self.querys = nn.ModuleList([
+            nn.Linear(in_features=word_size, out_features=embed_dim, bias=True)
+            for _ in range(n_query)
+        ])
+        self.key = nn.Linear(in_features=word_size, out_features=embed_dim, bias=True)
+        self.value = nn.Linear(in_features=word_size, out_features=embed_dim, bias=True)
+
+    def forward(self, x: Tensor) -> Tensor:
+        K = self.key(x)
+        V = self.value(x)
+        Z_s = torch.cat([
+            self.self_attention(query(x), K, V) for query in self.querys
+        ], dim=1)
+        Z = torch.matmul(Z_s, self.W0)
+        return Z
+
+def test_forward_MultiQueryAttention():
+    word_size =512
+    device = torch.device('cuda', 0)
+    mqa = MultiQueryAttention(word_size=word_size, embed_dim=64, n_query=8).to(device=device)
+
+    # Tạo các embedding của 3 từ
+    word1 = torch.randn(1, word_size)  # Embedding của từ thứ nhất
+    word2 = torch.randn(1, word_size)  # Embedding của từ thứ hai
+    word3 = torch.randn(1, word_size)  # Embedding của từ thứ ba
+        # Gộp các embedding thành một tensor đầu vào
+    input_tensor = torch.cat([word1, word2, word3], dim=0).to(device=device)
+    output = mqa(input_tensor)
+    print(output)
+    print(output.shape)
